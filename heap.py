2d050884e0111ac1feaead67571ff65ad0d6925d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-18 16:41:54 -0500

    Assume largest element is the parent node; Initialize largest var to parent indx.;

--- heap.py
+++ heap.py
@@ -3,3 +3,5 @@ print("Heapsort!")
 # Function to create Max-Heap
 # Takes three Params: array, array indx, and array size
 def heapify (array, i, size):
+	# Assume largest element is the parent node; Initialize largest var to parent indx.
+	largest = i
