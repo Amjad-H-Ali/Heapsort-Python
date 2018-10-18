1d4df61f0fb4aa7c2b0783a32e3a0bebc3b6908c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-18 16:43:18 -0500

    Right child node indx.;

--- heap.py
+++ heap.py
@@ -6,6 +6,8 @@ def heapify (array, i, size):
 	# Assume largest element is the parent node; Initialize largest var to parent indx.
 	largest = i
 
-	# Left child node indx
+	# Left child node indx.
 	l = (i * 2) + 1
-	
+
+	# Right child node indx.
+	r = (i * 2) + 2
